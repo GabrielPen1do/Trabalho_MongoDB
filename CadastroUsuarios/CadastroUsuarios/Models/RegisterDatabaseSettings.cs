d5d0cfcd36250ea08a1f08f28644c517e0b9e580
GabrielPen1do <gabriel.jesus.1463505@sga.pucminas.br>
2024-10-14 18:17:39 -0300

    creating models

---
+++ CadastroUsuarios/CadastroUsuarios/Models/RegisterDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace CadastroUsuarios.Models
+{
+	public class RegisterDatabaseSettings
+	{
+		public string ConnectionString { get; set; } = null!;
+
+		public string DatabaseName { get; set; } = null!;
+
+		public string RegisterCollectionName { get; set; } = null!;
+	}
+}
---
+++ CadastroUsuarios/CadastroUsuarios/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CadastroUsuarios.Models
+{
+	public class Users
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; } = String.Empty;
+		public string Nome { get; set; } = String.Empty;
+		public string Email { get; set; } = String.Empty;
+		public string Telefone { get; set; } = String.Empty;
+	}
+}
