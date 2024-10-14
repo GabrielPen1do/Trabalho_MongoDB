eaa551ff4807a372b09df07516927349f8e0fbce
GabrielPen1do <gabriel.jesus.1463505@sga.pucminas.br>
2024-10-14 18:21:45 -0300

    development of the controller

---
+++ CadastroUsuarios/CadastroUsuarios/Controllers/UsersController.cs
@@ -0,0 +1,115 @@
+using CadastroUsuarios.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CadastroUsuarios.Controllers
+{
+	public class UsersController : Controller
+	{
+
+		private readonly RegisterService _registerService;
+
+		public UsersController(RegisterService registerService) =>
+			_registerService = registerService;
+
+		public async Task<IActionResult> TotalUsersCount()
+		{
+			var count = await _registerService.GetTotalUsersCountAsync(); // Implementar este método no RegisterService
+			return Json(new { Count = count });
+		}
+
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var users = await _registerService.GetAsync();
+			return View(users);
+		}
+
+
+		[HttpGet("{id:length(24)}")]
+		public async Task<ActionResult> Details(string id)
+		{
+			var users = await _registerService.GetAsync(id);
+
+			if (users is null)
+			{
+				return NotFound();
+			}
+
+			return View();
+		}
+
+		[HttpGet("create")]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+
+		[HttpPost("create")]
+		public async Task<IActionResult> Create(Users newUser)
+		{
+
+			if (!ModelState.IsValid)
+			{
+				return View(newUser);
+			}
+			await _registerService.CreateAsync(newUser);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		[HttpGet("edit/{id:length(24)}")]
+		public async Task<IActionResult> Edit(string id)
+		{
+			var user = await _registerService.GetAsync(id);
+
+			if (user is null)
+			{
+				return NotFound();
+			}
+
+			return View(user);
+		}
+
+
+		[HttpPost("edit/{id:length(24)}")]
+		public async Task<IActionResult> Edit(string id, Users updateUser)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(updateUser);
+			}
+
+			var user = await _registerService.GetAsync(id);
+
+			if (user is null)
+			{
+				return NotFound();
+			}
+
+			updateUser.Id = user.Id;
+
+			await _registerService.UpdateAsync(id, updateUser);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		[HttpPost("delete/{id:length(24)}")]
+		public async Task<IActionResult> Delete(string id)
+		{
+			var user = await _registerService.GetAsync(id);
+
+			if (user is null)
+			{
+				return NotFound();
+			}
+
+			await _registerService.RemoveAsync(id);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+	}
+}
---
+++ CadastroUsuarios/CadastroUsuarios/Services/RegisterService.cs
@@ -0,0 +1,57 @@
+﻿using CadastroUsuarios.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace CadastroUsuarios.Services
+{
+	public class RegisterService
+	{
+		private readonly IMongoCollection<Users> _userCollection;
+
+		public RegisterService(
+		   IOptions<RegisterDatabaseSettings> registerDatabaseSettings)
+		{
+			var mongoClient = new MongoClient(
+				registerDatabaseSettings.Value.ConnectionString);
+
+			var mongoDatabase = mongoClient.GetDatabase(
+				registerDatabaseSettings.Value.DatabaseName);
+
+			_userCollection = mongoDatabase.GetCollection<Users>(
+				registerDatabaseSettings.Value.RegisterCollectionName);
+		}
+
+		public async Task<int> GetTotalUsersCountAsync()
+		{
+			return (int)await _userCollection.CountDocumentsAsync(new BsonDocument());
+		}
+
+		public async Task<long> CountTotalUsersAsync() =>
+		await _userCollection.CountDocumentsAsync(_ => true);
+
+		public async Task CreateAsync(Users user) =>
+
+			await _userCollection.InsertOneAsync(user);
+
+		public async Task<List<Users>> GetAsync() =>
+		 await _userCollection.Find(_ => true).ToListAsync();
+
+
+		public async Task<Users?> GetAsync(string id) =>
+
+		 await _userCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+
+		public async Task RemoveAsync(string id) =>
+
+		   await _userCollection.DeleteOneAsync(x => x.Id == id);
+
+
+		public async Task UpdateAsync(string id, Users user)
+		{
+			await _userCollection.ReplaceOneAsync(x => x.Id == id, user);
+		}
+
+	}
+}
